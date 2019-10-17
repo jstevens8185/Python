a864c3ac0b46bac261b60f4a40a6e4118775662b
Jered Stevens <jstevens8185@gmail.com>
2019-10-17 18:24:13 -0500

    displays a message

---
+++ Ch2Var&simpleData/PersonalMessage/PmMe.py
@@ -0,0 +1,8 @@
+#saves a name to a variable then prints a message
+#containing that variable
+
+name = "Jered"
+hello = "Hello "
+message = ". It's Jacky Vincent. Would you like to open for us?"
+allOfIt = f"{hello}{name}{message}"
+print(allOfIt)
